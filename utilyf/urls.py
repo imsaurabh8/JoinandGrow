44ae17f67213771d1f7f417cc4dfc05793cbfa39
imsaurabh8 <43519993+imsaurabh8@users.noreply.github.com>
2021-07-25 18:54:16 +0530

    Update urls.py

--- utilyf/urls.py
+++ utilyf/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path("about/",views.about,name="about"),
 path("",views.index,name="index"),
 ] + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
++ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
 urlpatterns += staticfiles_urlpatterns()
 
